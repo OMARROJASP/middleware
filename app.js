5be07e998514c7f537a2954b651b61609e305367
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-03-10 19:16:34 -0500

    manejo de error

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const express = require('express')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware ejecutado');
-    next();
+
+// app.use('/error', (req, res, next) => {
+//     const error = new Error('Este es un error de prueba');
+//     next(error);
+// })
+
+// app.use((err, req, res, next) => {
+//     console.error(err, stack);
+//     res.status(500).send('Algo salio mal');
+// })
+// error con try-catch
+// app.get('/async-error', async(req, res, next) => {
+//     try{
+//         throw new Error('Error asincronico');
+//     }catch(error){
+//         next(error)
+//     }
+// })
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    if(err.message === 'No encontrado'){
+        return res.status(404).json({
+            massage: 'Recurso no encontrad',
+            error: err.message,
+        })
+    }
+
+    res.status(500).json({
+        message: 'Algo salió mal',
+        error: err.message,
+    })
 })
 
-app.use('/carb', (req, res, next) => {
-    console.log("Middleware solo para prpgramadores");
-    next()
-});
 
-app.get('/', (req, res) => {
-    res.send("Hola middleware")
-})
+
+
 
 app.listen(3000, () => {
     console.log("Servidor en http:// localhost:3000")
